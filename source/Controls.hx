8d4081917316add33c9cb06f45be962cad6b40bd
Random people <98900217+justaguy6@users.noreply.github.com>
2023-06-15 09:22:30 -0300

    Update Controls.hx

--- source/Controls.hx
+++ source/Controls.hx
@@ -5,6 +5,7 @@ import flixel.input.FlxInput;
 import flixel.input.actions.FlxAction;
 import flixel.input.actions.FlxActionInput;
 import flixel.input.actions.FlxActionManager;
+import flixel.input.actions.FlxActionInputDigital;
 import flixel.input.actions.FlxActionSet;
 import flixel.input.gamepad.FlxGamepadInputID;
 import flixel.input.keyboard.FlxKey;
