1d5fb8131c8ca10ea3f6f962436bfaf7f50a54a2
Random people <98900217+justaguy6@users.noreply.github.com>
2023-06-16 07:31:20 -0300

    Create FatalError.hx

---
+++ source/FatalError.hx
@@ -0,0 +1,65 @@
+package;
+
+import flixel.FlxG;
+import flixel.FlxSprite;
+import flixel.FlxSubState;
+import flixel.text.FlxText;
+import flixel.util.FlxColor;
+import flixel.effects.FlxFlicker;
+import lime.app.Application;
+import flixel.addons.transition.FlxTransitionableState;
+import flixel.tweens.FlxTween;
+import flixel.util.FlxTimer;
+
+class FatalError extends MusicBeatState
+{
+	public static var leftState:Bool = false;
+
+	var warnText:FlxText;
+	override function create()
+	{
+		super.create();
+
+		var bg:FlxSprite = new FlxSprite().makeGraphic(FlxG.width, FlxG.height, FlxColor.BLACK);
+		add(bg);
+
+		warnText = new FlxText(0, 0, FlxG.width,
+			"opa, parece que o sistema detectou que os créditos foram removidos
+       voce não vai poder jogar sem eles, lamentamos, pressione A para continuar",
+			32);
+		warnText.setFormat("VCR OSD Mono", 32, FlxColor.WHITE, CENTER);
+		warnText.screenCenter(Y);
+		add(warnText);
+	}
+
+	override function update(elapsed:Float)
+	{
+		if(!leftState) {
+			var back:Bool = controls.BACK;
+			if (controls.ACCEPT || back) {
+				leftState = true;
+				FlxTransitionableState.skipNextTransIn = true;
+				FlxTransitionableState.skipNextTransOut = true;
+				if(!back) {
+					ClientPrefs.flashing = false;
+					ClientPrefs.saveSettings();
+					FlxG.sound.play(Paths.sound('confirmMenu'));
+					FlxFlicker.flicker(warnText, 1, 0.1, false, true, function(flk:FlxFlicker) {
+						new FlxTimer().start(0.5, function (tmr:FlxTimer) {
+							// anti ladrão lol
+							System.exit(0); // jogo morre lindamente 
+						});
+					});
+				} else {
+					FlxG.sound.play(Paths.sound('cancelMenu'));
+					FlxTween.tween(warnText, {alpha: 0}, 1, {
+						onComplete: function (twn:FlxTween) {
+							MusicBeatState.switchState(new TitleState());
+						}
+					});
+				}
+			}
+		}
+		super.update(elapsed);
+	}
+}
